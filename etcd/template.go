c84dd6509b0c55068ff6eeb5c76f3de29b858326
李学坪 <lixueping@lixuepingdeiMac.local>
2017-12-05 16:23:31 +0800

    为service字段新增template

--- etcd/template.go
+++ etcd/template.go
@@ -22,3 +22,19 @@ func GetEtcdTemplate() []map[string]interface{} {
 	)
 	return column
 }
+
+func GetEtcdServiceTemplate() []map[string]interface{} {
+	column := []map[string]interface{}{}
+	column = append(column,
+		//map[string]interface{}{"field":"state","checkbox":true,"align":"center","valign":"middle"},
+		map[string]interface{}{"field":"id","title":"Key","sortable":"true","align":"center","valign":"middle"},
+		map[string]interface{}{"field":"status","title":"状态","sortable":"true","align":"center","valign":"middle"},
+		map[string]interface{}{"field":"value","title":"Value","sortable":"true","align":"center","valign":"middle"},
+		map[string]interface{}{"field":"version","title":"version","sortable":"true","align":"center","valign":"middle"},
+		map[string]interface{}{"field":"lease","title":"Lease","sortable":"true","align":"center","valign":"middle"},
+		map[string]interface{}{"field":"createrevision","title":"CreateRevision","sortable":"true","align":"center","valign":"middle"},
+		map[string]interface{}{"field":"moderevision","title":"ModRevision","sortable":"true","align":"center","valign":"middle"},
+		map[string]interface{}{"field":"op","title":"操作","sortable":"false","align":"center","valign":"middle"},
+	)
+	return column
+}
