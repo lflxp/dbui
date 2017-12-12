38fe39559792ef88934c501d9eaedcae8e2171b3
lflxp <lxp@lflxp.cn>
2017-12-13 01:17:46 +0800

    新增docker字段

--- etcd/etcd.go
+++ etcd/etcd.go
@@ -4,30 +4,31 @@ import (
 	"fmt"
 	//_ "github.com/lflxp/databases/routers"
 	//"github.com/astaxie/beego"
-	"github.com/coreos/etcd/clientv3"
-	"github.com/coreos/etcd/client"
-	"github.com/coreos/etcd/etcdserver/api/v3rpc/rpctypes"
-	"time"
-	"net"
 	"context"
-	"strings"
 	"errors"
+	"net"
+	"strings"
+	"time"
+
+	"github.com/coreos/etcd/client"
+	"github.com/coreos/etcd/clientv3"
+	"github.com/coreos/etcd/etcdserver/api/v3rpc/rpctypes"
 )
 
 type EtcdUi struct {
-	Endpoints 	[]string  //[]string{"localhost:2379"}
-	ClientConn 	*clientv3.Client
+	Endpoints    []string //[]string{"localhost:2379"}
+	ClientConn   *clientv3.Client
 	ClientConnV2 client.Client
-	Tree 		[]map[string]string
-	TopName 	[]string 	//顶级数据库名
-	Version 	string  //2 or 3
+	Tree         []map[string]string
+	TopName      []string //顶级数据库名
+	Version      string   //2 or 3
 }
 
-func (this *EtcdUi) Remove(s []string,de string) []string {
+func (this *EtcdUi) Remove(s []string, de string) []string {
 	tmp := []string{}
-	for _,key := range s {
+	for _, key := range s {
 		if key != de {
-			tmp = append(tmp,key)
+			tmp = append(tmp, key)
 		}
 	}
 	return tmp
@@ -37,26 +38,26 @@ func (this *EtcdUi) Remove(s []string,de string) []string {
 //TopName目前只支持单数据库 多数据库以后写
 func (this *EtcdUi) GetTopic(data []string) []string {
 	tmp := data
-	for _,key := range data {
-		for _,k2 := range data {
+	for _, key := range data {
+		for _, k2 := range data {
 			if key != k2 {
 				//如果key短值是k2长值得开头(key比k2短，但是key和k2得值是包含关系)
-				if strings.HasPrefix(k2,key) && strings.Contains(k2,key) {
+				if strings.HasPrefix(k2, key) && strings.Contains(k2, key) {
 					//fmt.Println("##############TOP ",k2,key)
-					tmp = this.Remove(tmp,k2)
+					tmp = this.Remove(tmp, k2)
 				}
 			}
 		}
 	}
-	this.TopName = []string{"ETCD->"+strings.Join(this.Endpoints,"<-")}
+	this.TopName = []string{"ETCD->" + strings.Join(this.Endpoints, "<-")}
 	//将连接作为顶级域名
-	this.Tree = append(this.Tree,map[string]string{"name":this.TopName[0],"parentOrg":"null"})
-	for _,k := range tmp {
+	this.Tree = append(this.Tree, map[string]string{"name": this.TopName[0], "parentOrg": "null"})
+	for _, k := range tmp {
 		ttt := map[string]string{}
 		ttt["name"] = k
 		ttt["value"] = k
 		ttt["parentOrg"] = this.TopName[0]
-		this.Tree = append(this.Tree,ttt)
+		this.Tree = append(this.Tree, ttt)
 	}
 	return tmp
 }
@@ -67,8 +68,8 @@ func (this *EtcdUi) HasKeyByTree(key string) bool {
 	if len(this.Tree) == 0 {
 		return false
 	}
-	for _,k := range this.Tree {
-		if value,ok := k["name"]; ok {
+	for _, k := range this.Tree {
+		if value, ok := k["name"]; ok {
 			if value == key {
 				rs = true
 			}
@@ -80,30 +81,48 @@ func (this *EtcdUi) HasKeyByTree(key string) bool {
 //根据顶级目录 获取所有子目录
 func (this *EtcdUi) GetLastData(key string) {
 	last := this.More(key)
-	for _,y := range last.Kvs {
+	for _, y := range last.Kvs {
 		if string(y.Key) != key {
 			tmp := map[string]string{}
 			//fmt.Println("getLastData",string(y.Key))
 			if this.HasChildTree(string(y.Key)) {
 				if !this.HasKeyByTree(string(y.Key)) {
 					//fmt.Println("has more",string(y.Key),key)
+					st1 := string(y.Value)
+					st2 := strings.Split(st1, "::")
 					tmp["name"] = string(y.Key)
-					tmp["value"] = string(y.Value)
-					tmp["ttl"] = fmt.Sprintf("%d",y.Lease)
-					tmp["version"] = fmt.Sprintf("%d",y.Version)
+					tmp["value"] = st1
+					tmp["did"] = st2[0]
+					tmp["dimage"] = st2[1]
+					tmp["dcommand"] = st2[2]
+					tmp["dstate"] = st2[3]
+					tmp["dStatus"] = st2[4]
+					tmp["dports"] = st2[5]
+					tmp["dname"] = st2[6]
+					tmp["ttl"] = fmt.Sprintf("%d", y.Lease)
+					tmp["version"] = fmt.Sprintf("%d", y.Version)
 					tmp["parentOrg"] = key
-					this.Tree = append(this.Tree,tmp)
+					this.Tree = append(this.Tree, tmp)
 				}
 				this.GetLastData(string(y.Key))
 			} else {
 				if !this.HasKeyByTree(string(y.Key)) {
 					//fmt.Println("no more",string(y.Key),string(y.Value),key)
+					st1 := string(y.Value)
+					st2 := strings.Split(st1, "::")
 					tmp["name"] = string(y.Key)
-					tmp["value"] = string(y.Value)
-					tmp["ttl"] = fmt.Sprintf("%d",y.Lease)
-					tmp["version"] = fmt.Sprintf("%d",y.Version)
+					tmp["value"] = st1
+					tmp["did"] = st2[0]
+					tmp["dimage"] = st2[1]
+					tmp["dcommand"] = st2[2]
+					tmp["dstate"] = st2[3]
+					tmp["dStatus"] = st2[4]
+					tmp["dports"] = st2[5]
+					tmp["dname"] = st2[6]
+					tmp["ttl"] = fmt.Sprintf("%d", y.Lease)
+					tmp["version"] = fmt.Sprintf("%d", y.Version)
 					tmp["parentOrg"] = key
-					this.Tree = append(this.Tree,tmp)
+					this.Tree = append(this.Tree, tmp)
 				}
 			}
 		}
@@ -112,8 +131,8 @@ func (this *EtcdUi) GetLastData(key string) {
 
 func (this *EtcdUi) HasChildTree(key string) bool {
 	var status bool
-	ctx,cancel := context.WithTimeout(context.Background(),5*time.Second)
-	resp,err := this.ClientConn.Get(ctx,key,clientv3.WithPrefix())
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	resp, err := this.ClientConn.Get(ctx, key, clientv3.WithPrefix())
 	cancel()
 	if err != nil {
 		//fmt.Println(err.Error())
@@ -127,11 +146,10 @@ func (this *EtcdUi) HasChildTree(key string) bool {
 	return status
 }
 
-
 //more 是底层吗
 func (this *EtcdUi) More(data string) *clientv3.GetResponse {
-	ctx,cancel := context.WithTimeout(context.Background(),5*time.Second)
-	resp,err := this.ClientConn.Get(ctx,data,clientv3.WithPrefix())
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	resp, err := this.ClientConn.Get(ctx, data, clientv3.WithPrefix())
 	cancel()
 	if err != nil {
 		//fmt.Println(err.Error())
@@ -142,8 +160,8 @@ func (this *EtcdUi) More(data string) *clientv3.GetResponse {
 
 //more 是底层吗
 func (this *EtcdUi) Get(data string) *clientv3.GetResponse {
-	ctx,cancel := context.WithTimeout(context.Background(),5*time.Second)
-	resp,err := this.ClientConn.Get(ctx,data)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	resp, err := this.ClientConn.Get(ctx, data)
 	cancel()
 	if err != nil {
 		//fmt.Println(err.Error())
@@ -160,7 +178,7 @@ func (this *EtcdUi) FindData(data string) map[string]interface{} {
 	this.InitClientConn()
 	resp := this.More(data)
 	TotalRs["total"] = resp.Count
-	for _,key := range resp.Kvs {
+	for _, key := range resp.Kvs {
 		tmp := map[string]interface{}{}
 		tmp["id"] = string(key.Key)
 		tmp["value"] = string(key.Value)
@@ -171,8 +189,8 @@ func (this *EtcdUi) FindData(data string) map[string]interface{} {
 		tmp["memberid"] = resp.Header.MemberId
 		tmp["ClusterId"] = resp.Header.ClusterId
 		tmp["RaftTerm"] = resp.Header.RaftTerm
-		tmp["op"] = fmt.Sprintf("<a href=\"#passwd\" data-toggle=\"modal\" id=\"install\" class=\"btn btn-success btn-sm\"><i class=\"glyphicon glyphicon-wrench\"></i>修改</a><button onclick=\"Delete('%s')\" class=\"btn btn-danger btn-sm\"><i class=\"glyphicon glyphicon-remove\"></i> 删除 </button>",string(key.Key)) 
-		result = append(result,tmp)
+		tmp["op"] = fmt.Sprintf("<a href=\"#passwd\" data-toggle=\"modal\" id=\"install\" class=\"btn btn-success btn-sm\"><i class=\"glyphicon glyphicon-wrench\"></i>修改</a><button onclick=\"Delete('%s')\" class=\"btn btn-danger btn-sm\"><i class=\"glyphicon glyphicon-remove\"></i> 删除 </button>", string(key.Key))
+		result = append(result, tmp)
 	}
 	TotalRs["rows"] = result
 	return TotalRs
@@ -180,8 +198,8 @@ func (this *EtcdUi) FindData(data string) map[string]interface{} {
 
 //more 是底层吗
 func (this *EtcdUi) Count(data string) bool {
-	ctx,cancel := context.WithTimeout(context.Background(),5*time.Second)
-	resp,err := this.ClientConn.Get(ctx,data,clientv3.WithPrefix())
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	resp, err := this.ClientConn.Get(ctx, data, clientv3.WithPrefix())
 	cancel()
 	if err != nil {
 		//fmt.Println(err.Error())
@@ -193,9 +211,9 @@ func (this *EtcdUi) Count(data string) bool {
 //get etcd clientV3 conn
 func (this *EtcdUi) InitClientConn() {
 	if this.Version != "2" {
-		cli,err := clientv3.New(clientv3.Config{
-			Endpoints:this.Endpoints,
-			DialTimeout:5*time.Second,
+		cli, err := clientv3.New(clientv3.Config{
+			Endpoints:   this.Endpoints,
+			DialTimeout: 5 * time.Second,
 		})
 		if err != nil {
 			//fmt.Println(err.Error())
@@ -204,21 +222,21 @@ func (this *EtcdUi) InitClientConn() {
 		this.ClientConn = cli
 	} else {
 		cfg := client.Config{
-			Endpoints: this.Endpoints,
-			Transport: client.DefaultTransport,
+			Endpoints:               this.Endpoints,
+			Transport:               client.DefaultTransport,
 			HeaderTimeoutPerRequest: 5 * time.Second,
 		}
-		c,err := client.New(cfg)
+		c, err := client.New(cfg)
 		if err != nil {
 			panic(err)
 		}
 		this.ClientConnV2 = c
 		kapi := client.NewKeysAPI(c)
-		resp,err := kapi.Get(context.Background(),"/asd",&client.GetOptions{Recursive:true,})
+		resp, err := kapi.Get(context.Background(), "/asd", &client.GetOptions{Recursive: true})
 		if err != nil {
 			panic(err)
 		}
-		fmt.Println(resp.Node.Key,resp.Node.Value)
+		fmt.Println(resp.Node.Key, resp.Node.Value)
 	}
 }
 
@@ -228,8 +246,8 @@ func (this *EtcdUi) Close() {
 
 //endpoint []string{"localhost:2379"}
 func (this *EtcdUi) GetAllDatas() []string {
-	ctx,cancel := context.WithTimeout(context.Background(),5*time.Second)
-	resp,err := this.ClientConn.Get(ctx,"",clientv3.WithPrefix())
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	resp, err := this.ClientConn.Get(ctx, "", clientv3.WithPrefix())
 	cancel()
 	if err != nil {
 		//fmt.Println(err.Error())
@@ -237,12 +255,12 @@ func (this *EtcdUi) GetAllDatas() []string {
 	}
 	//fmt.Println(resp.More)
 	rs := []string{}
-	for _,ev := range resp.Kvs {
+	for _, ev := range resp.Kvs {
 		//fmt.Println(string(ev.Key))
 		//fmt.Println(fmt.Printf("%d %s %s\n",ev.Lease,ev.Key,ev.Value))
 		//fmt.Println(ev.String())
 		//rs[string(ev.Key)] = string(ev.Value)
-		rs = append(rs,string(ev.Key))
+		rs = append(rs, string(ev.Key))
 	}
 	return rs
 }
@@ -250,29 +268,29 @@ func (this *EtcdUi) GetAllDatas() []string {
 //快速获取tree所有信息
 func (this *EtcdUi) GetAllTreeRelate() {
 	this.InitClientConn()
-	for _,key := range this.GetTopic(this.GetAllDatas()) {
+	for _, key := range this.GetTopic(this.GetAllDatas()) {
 		this.GetLastData(key)
 	}
 }
 
 //递归函数，获得树形结构的关系信息 tree-view
-func (this *EtcdUi) GetTreeRelate(top []string,all []map[string]string) string {
+func (this *EtcdUi) GetTreeRelate(top []string, all []map[string]string) string {
 	result := []string{}
-	for _,y := range top {
-		result = append(result,"{text:'"+strings.Split(y,"/")[len(strings.Split(y,"/"))-1]+"'")
-		if this.HasChild(y,all) {
-			result = append(result,"selectable:true,multiSelect:false,state:{expanded:false,disabled:false},href:'#',ids:'"+y+"','nodes':["+this.GetTreeRelate(this.ForeignKeys(y,all),all)+"]}")
+	for _, y := range top {
+		result = append(result, "{text:'"+strings.Split(y, "/")[len(strings.Split(y, "/"))-1]+"'")
+		if this.HasChild(y, all) {
+			result = append(result, "selectable:true,multiSelect:false,state:{expanded:false,disabled:false},href:'#',ids:'"+y+"','nodes':["+this.GetTreeRelate(this.ForeignKeys(y, all), all)+"]}")
 		} else {
-			result = append(result,"icon:'glyphicon glyphicon-list-alt',selectable:true,href:'#',ids:'"+y+"'}")
+			result = append(result, "icon:'glyphicon glyphicon-list-alt',selectable:true,href:'#',ids:'"+y+"'}")
 		}
 	}
-	return strings.Join(result,",")
+	return strings.Join(result, ",")
 }
 
 //判断是否还有子机构
-func (this *EtcdUi) HasChild(id string,data []map[string]string) bool {
+func (this *EtcdUi) HasChild(id string, data []map[string]string) bool {
 	ok := false
-	for _,y := range data {
+	for _, y := range data {
 		if y["parentOrg"] == id {
 			ok = true
 		}
@@ -281,17 +299,16 @@ func (this *EtcdUi) HasChild(id string,data []map[string]string) bool {
 }
 
 //获取所有上级机构为key的子机构（第二层，用于下面的递归）
-func (this *EtcdUi) ForeignKeys(key string,data []map[string]string) []string {
+func (this *EtcdUi) ForeignKeys(key string, data []map[string]string) []string {
 	res := []string{}
-	for _,y := range data {
+	for _, y := range data {
 		if y["parentOrg"] == key {
 			// fmt.Println("HasChild",y["parentOrg"],key,y["name"])
-			res = append(res,y["name"])
+			res = append(res, y["name"])
 		}
 	}
 	return res
 }
-
 
 //根据顶级机构和所有数据进行递归 得到树形结构的json字符串
 //获取所有tree table最终数据
@@ -300,9 +317,9 @@ func (this *EtcdUi) GetTreeByString() string {
 		defer this.Close()
 		this.GetAllTreeRelate()
 		//return "["+this.GetTreeRelate(this.GetTopic(this.GetAllDatas()),this.Tree)+"]"
-		return "["+this.GetTreeRelate(this.TopName,this.Tree)+"]"
+		return "[" + this.GetTreeRelate(this.TopName, this.Tree) + "]"
 	}
-	return fmt.Sprintf("%s Unreachable",this.Endpoints[0])	
+	return fmt.Sprintf("%s Unreachable", this.Endpoints[0])
 }
 
 func (this *EtcdUi) ScannerPort(ipAndPort string) bool {
@@ -317,94 +334,94 @@ func (this *EtcdUi) ScannerPort(ipAndPort string) bool {
 }
 
 //CRUD
-func (this *EtcdUi) AddLease(key,value string, ttl int64) error {
+func (this *EtcdUi) AddLease(key, value string, ttl int64) error {
 	if this.ScannerPort(this.Endpoints[0]) {
 		this.InitClientConn()
 		defer this.Close()
 
-		resp,err := this.ClientConn.Grant(context.TODO(),ttl)
+		resp, err := this.ClientConn.Grant(context.TODO(), ttl)
 		if err != nil {
 			return err
 		}
-	
-		ctx,cancel := context.WithTimeout(context.Background(),5*time.Second)
-		_,err = this.ClientConn.Put(ctx,key,value,clientv3.WithLease(resp.ID))
+
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		_, err = this.ClientConn.Put(ctx, key, value, clientv3.WithLease(resp.ID))
 		cancel()
 		if err != nil {
-				switch err {
-				case context.Canceled:
-					fmt.Printf("ctx is canceled by another routine: %v\n", err)
-				case context.DeadlineExceeded:
-					fmt.Printf("ctx is attached with a deadline is exceeded: %v\n", err)
-				case rpctypes.ErrEmptyKey:
-					fmt.Printf("client-side error: %v\n", err)
-				default:
-					fmt.Printf("bad cluster endpoints, which are not etcd servers: %v\n", err)
-				}
+			switch err {
+			case context.Canceled:
+				fmt.Printf("ctx is canceled by another routine: %v\n", err)
+			case context.DeadlineExceeded:
+				fmt.Printf("ctx is attached with a deadline is exceeded: %v\n", err)
+			case rpctypes.ErrEmptyKey:
+				fmt.Printf("client-side error: %v\n", err)
+			default:
+				fmt.Printf("bad cluster endpoints, which are not etcd servers: %v\n", err)
 			}
-		return err	
+		}
+		return err
 	}
-	return errors.New(fmt.Sprintf("%s unreachable",this.Endpoints[0]))
+	return errors.New(fmt.Sprintf("%s unreachable", this.Endpoints[0]))
 }
 
 //CRUD
-func (this *EtcdUi) Add(key,value string) error {
+func (this *EtcdUi) Add(key, value string) error {
 	if this.ScannerPort(this.Endpoints[0]) {
 		this.InitClientConn()
 		defer this.Close()
-	
-		ctx,cancel := context.WithTimeout(context.Background(),5*time.Second)
-		_,err := this.ClientConn.Put(ctx,key,value)
+
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		_, err := this.ClientConn.Put(ctx, key, value)
 		cancel()
 		if err != nil {
-				switch err {
-				case context.Canceled:
-					fmt.Printf("ctx is canceled by another routine: %v\n", err)
-				case context.DeadlineExceeded:
-					fmt.Printf("ctx is attached with a deadline is exceeded: %v\n", err)
-				case rpctypes.ErrEmptyKey:
-					fmt.Printf("client-side error: %v\n", err)
-				default:
-					fmt.Printf("bad cluster endpoints, which are not etcd servers: %v\n", err)
-				}
+			switch err {
+			case context.Canceled:
+				fmt.Printf("ctx is canceled by another routine: %v\n", err)
+			case context.DeadlineExceeded:
+				fmt.Printf("ctx is attached with a deadline is exceeded: %v\n", err)
+			case rpctypes.ErrEmptyKey:
+				fmt.Printf("client-side error: %v\n", err)
+			default:
+				fmt.Printf("bad cluster endpoints, which are not etcd servers: %v\n", err)
 			}
-		return err	
+		}
+		return err
 	}
-	return errors.New(fmt.Sprintf("%s unreachable",this.Endpoints[0]))
+	return errors.New(fmt.Sprintf("%s unreachable", this.Endpoints[0]))
 }
 
 func (this *EtcdUi) Delete(key string) error {
 	if this.ScannerPort(this.Endpoints[0]) {
 		this.InitClientConn()
 		defer this.Close()
-		
-		ctx,cancel := context.WithTimeout(context.Background(),5*time.Second)
+
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 		defer cancel()
-		
-		_,err := this.ClientConn.Delete(ctx,key)
+
+		_, err := this.ClientConn.Delete(ctx, key)
 		if err != nil {
 			return err
 		}
 		return nil
 	}
-	return errors.New(fmt.Sprintf("%s unreachable",this.Endpoints[0]))
+	return errors.New(fmt.Sprintf("%s unreachable", this.Endpoints[0]))
 }
 
 func (this *EtcdUi) DeleteAll(key string) error {
 	if this.ScannerPort(this.Endpoints[0]) {
 		this.InitClientConn()
 		defer this.Close()
-		
-		ctx,cancel := context.WithTimeout(context.Background(),5*time.Second)
+
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 		defer cancel()
-		
-		_,err := this.ClientConn.Delete(ctx,key,clientv3.WithPrefix())
+
+		_, err := this.ClientConn.Delete(ctx, key, clientv3.WithPrefix())
 		if err != nil {
 			return err
 		}
 		return nil
 	}
-	return errors.New(fmt.Sprintf("%s unreachable",this.Endpoints[0]))
+	return errors.New(fmt.Sprintf("%s unreachable", this.Endpoints[0]))
 }
 
 /*
@@ -412,62 +429,62 @@ Jtopo 总关系图谱
 */
 //根据顶级机构和所有数据进行递归 得到树形结构的json字符串
 //获取所有tree table最终数据
-func (this *EtcdUi) GetTreeByMapJtopo() ([]map[string]interface{},error) {
+func (this *EtcdUi) GetTreeByMapJtopo() ([]map[string]interface{}, error) {
 	this.GetTreeByString()
 	// for k,v := range this.Tree {
 	// 	fmt.Println(k,v)
 	// }
-	
+
 	top := []string{}
-	for _,v := range this.Tree {
+	for _, v := range this.Tree {
 		if v["parentOrg"] == "null" {
-			top = append(top,v["name"])
-		}	
+			top = append(top, v["name"])
+		}
 	}
 	// fmt.Println("top",top)
-	rs := this.GetMapJtopo(top,this.Tree)
-	return rs,nil
+	rs := this.GetMapJtopo(top, this.Tree)
+	return rs, nil
 }
 
 //判断现有tree集合里面有key没有
 func (this *EtcdUi) HasKeyByTreeToGet(key string) (bool, map[string]string) {
 	rs := false
 	if len(this.Tree) == 0 {
-		return false,nil
+		return false, nil
 	}
-	for _,k := range this.Tree {
-		if value,ok := k["name"]; ok {
+	for _, k := range this.Tree {
+		if value, ok := k["name"]; ok {
 			if value == key {
 				rs = true
-				return rs,k
+				return rs, k
 			}
 		}
 	}
-	return rs,nil
+	return rs, nil
 }
 
 //根据map生成json数组
-func (this *EtcdUi) GetMapJtopo(top []string,all []map[string]string) []map[string]interface{} {
+func (this *EtcdUi) GetMapJtopo(top []string, all []map[string]string) []map[string]interface{} {
 	result := []map[string]interface{}{}
 	//获取顶级项目以及子项目
-	for _,y := range top {
+	for _, y := range top {
 		tmp := map[string]interface{}{}
-		tmp["name"] = strings.Split(y,"/")[len(strings.Split(y,"/"))-1]
+		tmp["name"] = strings.Split(y, "/")[len(strings.Split(y, "/"))-1]
 		//判断是否有子项目
-		if this.HasChild(y,this.Tree) {
-			tmp["nodes"] = this.GetMapJtopo(this.ForeignKeys(y,this.Tree),this.Tree)
+		if this.HasChild(y, this.Tree) {
+			tmp["nodes"] = this.GetMapJtopo(this.ForeignKeys(y, this.Tree), this.Tree)
 		} else {
-			if ok,data := this.HasKeyByTreeToGet(y); ok {
+			if ok, data := this.HasKeyByTreeToGet(y); ok {
 				tmp["ttl"] = data["ttl"]
 				tmp["version"] = data["version"]
-			} 
+			}
 			if tmp["value"] != nil {
-				tmp["value"] = fmt.Sprintf("%s,%s",tmp["value"].(string),y)
+				tmp["value"] = fmt.Sprintf("%s,%s", tmp["value"].(string), y)
 			} else {
 				tmp["value"] = y
 			}
 		}
-		result = append(result,tmp)
+		result = append(result, tmp)
 	}
 	return result
 }
